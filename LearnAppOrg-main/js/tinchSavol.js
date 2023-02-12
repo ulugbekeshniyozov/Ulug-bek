e44a65cdba0533e9eb0dd0feddc3e16f98ff82e6
ulugbekeshniyozov <koltspike44@gmail.com>
2023-02-12 18:48:49 +0500

    tinch

---
+++ LearnAppOrg-main/js/tinchSavol.js
@@ -0,0 +1,75 @@
+var Stext1 = "178,7";
+var Stext2 = "29";
+var Stext3 = "710";
+var Stext4 = "1752";
+var Stext5 = "Avstarliya";
+var Stext6 = "Antarktida";
+var Stext7 = "Alyaska";
+var Stext8 = "16000";
+var Stext9 = "20000";
+
+function savol(){
+    var inp_one = document.getElementById('tinch_text1');
+    var inp_two = document.getElementById('tinch_text2');
+    var inp_three = document.getElementById('tinch_text3');
+    var inp_four = document.getElementById('tinch_text4');
+    var inp_five = document.getElementById('tinch_text5');
+    var inp_six = document.getElementById('tinch_text6');
+    var inp_seven = document.getElementById('tinch_text7');
+    var inp_eight = document.getElementById('tinch_text8');
+    var inp_nine = document.getElementById('tinch_text9');
+
+    if(inp_one.value == Stext1){
+        inp_one.style.backgroundColor = " #6ab04c";
+    }else{
+        inp_one.style.backgroundColor = "#ff4d4d";
+    }
+
+    if(inp_two.value == Stext2){
+        inp_two.style.backgroundColor = " #6ab04c";
+    }else{
+        inp_two.style.backgroundColor = "#ff4d4d";
+    }
+
+    if(inp_three.value == Stext3){
+        inp_three.style.backgroundColor = " #6ab04c";
+    }else{
+        inp_three.style.backgroundColor = "#ff4d4d";
+    }
+
+    if(inp_four.value == Stext4){
+        inp_four.style.backgroundColor = " #6ab04c";
+    }else{
+        inp_four.style.backgroundColor = "#ff4d4d";
+    }
+
+    if(inp_five.value == Stext5){
+        inp_five.style.backgroundColor = " #6ab04c";
+    }else{
+        inp_five.style.backgroundColor = "#ff4d4d";
+    }
+
+    if(inp_six.value == Stext6){
+        inp_six.style.backgroundColor = " #6ab04c";
+    }else{
+        inp_six.style.backgroundColor = "#ff4d4d";
+    }
+
+    if(inp_seven.value == Stext7){
+        inp_seven.style.backgroundColor = " #6ab04c";
+    }else{
+        inp_seven.style.backgroundColor = "#ff4d4d";
+    }
+
+    if(inp_eight.value == Stext8){
+        inp_eight.style.backgroundColor = " #6ab04c";
+    }else{
+        inp_eight.style.backgroundColor = "#ff4d4d";
+    }
+    if(inp_nine.value == Stext9){
+        inp_nine.style.backgroundColor = " #6ab04c";
+    }else{
+        inp_nine.style.backgroundColor = "#ff4d4d";
+    }
+
+}
